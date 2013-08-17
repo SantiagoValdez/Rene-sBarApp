5cab3d527f570e11e8e172a61a30a01ad01d41dd
Santiago Valdez <santiago.kenshinvaldez@gmail.com>
2013-08-17 16:13:39 -0400

    Ignores

--- pruebasqlite.py
+++ pruebasqlite.py
@@ -12,9 +12,10 @@ for t in (('aa101', 'PRODUCTO 101', 445.00),
 ('bb202', 'PRODUCTO 202', 742.00),
 ('cc303', 'PRODUCTO 303', 457.00),
 ):
-          tabla.execute('insert into productos values (?,?,?)', t)
+	tabla.execute('insert into productos values (?,?,?)', t)
 
 conexion.commit()
 tabla.close()
 
 print "Hola negro"
+
